79e1e55e4fd296cb04f0e0852635f8c82aea6c65
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2014-03-16 17:21:44 +0530

    renamed

---
+++ lab2/First.java
@@ -0,0 +1,178 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+public class First extends HttpServlet  
+  {
+    		
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+					        throws IOException, ServletException // This method is called whenver we invoke this servlet. 
+	// Use "request" to read incoming HTTP headers
+	// Use "response" to specify the HTTP response line and headers
+  	{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter(); // Use "out" to send content to browser
+        out.println("<html>");
+        out.println("<body bgcolor=\"white\">");
+        out.println("<head>");
+        out.println("<title>  Movie DBMS  </title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h3 align=\"center\"> Movie Query </h3><hr>");
+	if(request.getParameter("SUBMIT")==null){     // value of SUBMIT parameter determines which link or url is used invoking servlet 
+			form1(request, response, out);
+	} 
+
+	else if(request.getParameter("SUBMIT").toString().equals("BACK")){
+				form1(request,response,out);    // Input Form
+	}
+	else {
+                form2(request, response, out);  // Result form
+        }
+	out.println("</body>");
+        out.println("</html>");
+        
+    }
+    	
+
+
+    	public void doPost(HttpServletRequest request,HttpServletResponse response)
+        	throws IOException, ServletException
+    	{
+        	doGet(request, response);
+    	}
+
+	public void form1(HttpServletRequest request,HttpServletResponse response,PrintWriter out) 
+	// This method gets invoked at start of servlet or whenever BACK is clicked from form2. This just shows the input form 
+	{
+		out.println("<P>");
+        out.print("<form action=\"");
+        out.print(response.encodeURL("First"));  // Here name of servlet to be invoked needs to be given
+        out.print("\" ");
+        out.println("method=POST>");
+		out.println("<label> Enter movie to find Director : </label>");    
+		out.println("<input type=\"text\" name=\"movie\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"getDirec\" ></br>");
+		out.println("<label> Enter actor to find movies : </label>");    
+		out.println("<input type=\"text\" name=\"actor\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"getActor\" ></br>");
+		out.println("<label> Enter year to find best movie of year : </label>");  
+		out.println("<input type=\"text\" name=\"rating\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"getMovie\" ></br>>"); 
+		out.println("<label> add new movie info : </label>");  
+		out.println("<input type=\"text\" name=\"anmi\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"anmi\" ><br/>");
+		out.println("<label> add new movie rating : </label>");  
+		out.println("<input type=\"text\" name=\"anmr\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"anmr\" ><br/>"); 
+		out.println("<label> change new movie info : </label>");  
+		out.println("<input type=\"text\" name=\"cnmi\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"cnmi\" ><br/>"); 
+		out.println("<label> change new movie rating : </label>");  
+		out.println("<input type=\"text\" name=\"cnmr\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"cnmr\" ><br/>"); 
+		out.println("<label> delete new movie info : </label>");  
+		out.println("<input type=\"text\" name=\"dnmi\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"dnmi\" ><br/>"); 
+		out.println("<label> delete new movie rating : </label>");  
+		out.println("<input type=\"text\" name=\"dnmr\" />"); 
+		out.println("<input type=submit name=\"SUBMIT\" value=\"dnmr\" ><br/>"); 
+		// All the buttons have same name "SUBMIT" but different value
+       	out.println("</form>");
+	}
+
+
+	public void form2(HttpServletRequest request,HttpServletResponse response,PrintWriter out) throws IOException
+		// This page displays result in new page 
+        // It contain link back to input form
+	{
+		String val;
+		String movie;
+		String Ope;
+		Ope = request.getParameter("SUBMIT");
+		if(Ope.equals("getDirec"))
+		{
+			movie = request.getParameter("movie");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.getDirector(movie);
+			out.println("<p>"+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if(Ope.equals("getActor"))
+		{
+			movie = request.getParameter("actor");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.getActors(movie);
+			out.println("<p>"+val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("getMovie"))
+		{
+			movie = request.getParameter("rating");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.getBestMovie(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("anmi"))
+		{
+			movie = request.getParameter("anmi");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.addMovieInfo(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("anmr"))
+		{
+			movie = request.getParameter("anmr");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.addMovierating(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("cnmi"))
+		{
+			movie = request.getParameter("cnmi");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.changeInfo(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("cnmr"))
+		{
+			movie = request.getParameter("cnmr");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.changeRating(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("dnmi"))
+		{
+			movie = request.getParameter("dnmi");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.removeMovieInfo(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+		else if (Ope.equals("dnmr"))
+		{
+			movie = request.getParameter("dnmi");
+			movieQuery queryWork = new movieQuery();
+			val = queryWork.removeMovieRating(movie);
+			out.println("<p> "+ val+"</p>");
+			out.println("<p><p align=center > <a href=First?SUBMIT=BACK>BACK </a>"); // This is way of passing parameters to the servlet. SUMBMIT is name of parameter and its value is "BACK"  
+
+		}
+
+
+
+	}
+}
---
+++ lab2/movieQuery.java
@@ -0,0 +1,445 @@
+import java.io.*;
+import java.util.*;
+
+class movieQuery
+{
+	static String movieRatings = "C:/dbms/tomcat/webapps/lab2/WEB-INF/classes/movieratings.csv" ;
+	static String movieinfo  = "C:/dbms/tomcat/webapps/lab2/WEB-INF/classes/movieinfo.csv";
+	Map<String,String> directorMovie = new HashMap<String,String>() ;
+	Map<String,List<String> > actorsMovie = new HashMap<String,List<String> >();
+	Map<String,List<String> > yearsMovie = new HashMap<String,List<String> >();
+	Map<String,String[] > movieVars = new HashMap<String,String[] >();
+	Map<String, Double > ratingMoive = new HashMap<String,Double> ();
+	void readMovieInfo() throws IOException 
+	 {
+		 File f = new File(movieinfo);
+		 BufferedReader in = null;
+		 try {
+			 	in = new BufferedReader(new FileReader(f));
+		 	 } 
+		 catch (FileNotFoundException e)
+		 {
+			 System.out.println("File is not present in the disk");
+			 e.printStackTrace();
+		 }
+		 
+		 String Line = in.readLine();
+		 while (Line != null)
+		 {
+			 String[] MovieVars = Line.split(":");
+			 /** Variables
+			  * 
+			  */
+			 String Title = MovieVars[0];
+			 String Director = MovieVars[1];
+			 String Year = MovieVars[2];
+			 //String Genre = MovieVars[3];
+			 String[] Actors = MovieVars[4].split(",");
+			 directorMovie.put(Title, Director);
+			 for(int i = 0;i < Actors.length;i++)
+			 {
+				 List<String> moviesList = actorsMovie.get(Actors[i].trim());
+				 
+				 if (moviesList != null)
+				 {
+					 moviesList.add(Title);
+					 actorsMovie.put((Actors[i]).trim(), moviesList);
+				 }
+				 else
+				 {
+					 List<String> movie = new ArrayList<String>();
+					 movie.add(Title);
+					 actorsMovie.put(Actors[i].trim(),movie);
+				 }
+
+			 }
+			 List<String> moviesYearList = yearsMovie.get(Year);
+			 if (moviesYearList != null)
+			 {
+				 moviesYearList.add(Title);
+				 yearsMovie.put(Year, moviesYearList);
+			 }
+			 else
+			 {
+				 List<String> movie = new ArrayList<String>();
+				 movie.add(Title);
+				 yearsMovie.put(Year,movie);
+			 }
+			 movieVars.put(Title,Line.split(":",2)[1].split(":"));
+			 
+			 Line = in.readLine();
+		 }
+		 
+		 in.close();
+	 }
+	void readMovieRatings() throws IOException 
+	 {
+		 File f = new File(movieRatings);
+		 BufferedReader in = null;
+		 try {
+			 	in = new BufferedReader(new FileReader(f));
+		 	 } 
+		 catch (FileNotFoundException e)
+		 {
+			 e.printStackTrace();
+		 }
+		 
+		 String Line = in.readLine();
+		 while (Line != null)
+		 {
+			 String[] MovieVars = Line.split(":");
+			 /** Variables
+			  * 
+			  */
+			 String Title = MovieVars[0];
+			 Double Rating =Double.parseDouble( MovieVars[1]);
+			 ratingMoive.put(Title,Rating);
+			 Line = in.readLine();
+		 }
+		 in.close();
+		 
+	 }
+
+	public movieQuery() throws IOException
+	{
+		readMovieInfo();
+		readMovieRatings();
+	}
+	 public String getActors(String Movie) throws IOException
+	 {
+	 	List<String> MoviesList = actorsMovie.get(Movie);
+	 	String ans = "";
+		 if(MoviesList != null)
+		 {
+			 for (int j = 0; j < MoviesList.size(); j++) 
+			 {
+				 ans= ans + "," + MoviesList.get(j);
+			 }
+			 return ans;
+		}
+		 else
+		 {
+			 return "We can't find the actor in our database";
+		 }
+
+	 }
+	static void writeContent(String content,String filename) throws IOException
+	 {
+		 FileWriter fw = new FileWriter(filename,true);
+		 BufferedWriter bw = new BufferedWriter(fw);
+		 content = content + "\n";
+		 bw.append(content);
+		 bw.close();
+	 }
+     /**
+      * Ability to add a new movie.
+      * @param args
+      * @throws IOException
+      */
+	 void addNewMovieInfo(String movie) throws IOException
+	 {
+		 String[] filmVars = movie.split(":");
+		 String Title = filmVars[0];
+		 String Director = filmVars[1];
+		 String Year = filmVars[2];
+		 //String Genre = filmVars[3];
+		 String[] Actors = filmVars[4].split(",");
+		 String check = directorMovie.get(Title);
+		 if(check == null)
+		 {
+			 directorMovie.put(Title, Director);
+			 for(int i = 0;i < Actors.length;i++)
+			 {
+				 List<String> moviesList = actorsMovie.get(Actors[i].trim());
+				 
+				 if (moviesList != null)
+				 {
+					 moviesList.add(Title);
+					 actorsMovie.put((Actors[i]).trim(), moviesList);
+					 //writing part
+				 }
+				 else
+				 {
+					 List<String> mov = new ArrayList<String>();
+					 mov.add(Title);
+					 actorsMovie.put(Actors[i].trim(),mov);
+				 }
+
+			 }
+			 writeContent(movie,movieinfo);
+
+			 List<String> moviesYearList = yearsMovie.get(Year);
+			 if (moviesYearList != null)
+			 {
+				 moviesYearList.add(Title);
+				 yearsMovie.put(Year, moviesYearList);
+			 }
+			 else
+			 {
+				 List<String> mov = new ArrayList<String>();
+				 mov.add(Title);
+				 yearsMovie.put(Year,mov);
+			 }
+			 movieVars.put(Title,movie.split(":",2)[1].split(":"));
+		 }
+		 else
+		 {
+
+		 }	 
+	 }
+	  String addNewMovieRating(String Line) throws IOException
+	 {
+		 String[] MovieVars = Line.split(":");
+		 /** Variables
+		  * 
+		  */
+		 String Title = MovieVars[0];
+		 Double Rating =Double.parseDouble( MovieVars[1]);
+		 Double checkTitle = ratingMoive.get(Title);
+		 if(checkTitle == null)
+		 {
+			 ratingMoive.put(Title,Rating);
+			 writeContent(Line,movieRatings);
+			 return "Movie added to database";
+			 
+		 }
+		 else
+		 {
+			 return "The movie already exist in our database.";
+		 }
+
+	 }
+	 @SuppressWarnings("unused")
+	String removeMovie(String Title,String filename) throws IOException
+	 {
+		 File f = new File(filename);
+		 BufferedReader in = null;
+		 try {
+			 	in = new BufferedReader(new FileReader(f));
+		 	 } 
+		 catch (FileNotFoundException e)
+		 {
+			 return "File is not present in the disk";
+		 }
+		 
+		 String Line = in.readLine();
+		 int i =1;
+		 while (Line != null)
+		 {
+			 String[] Vars = Line.split(":",2);
+			 if(Vars[0].equals(Title))
+			 {
+				 in.close();
+				 removeLineFromFile(filename,Line);
+				 return "movie data removed succesfully";
+			 }
+			 i++;
+			 Line = in.readLine();
+			 if(Line == null)
+			 {
+				 return "Movie doesn't exist in our database";
+			 }
+		 }
+		 in.close();
+		 
+		 return "Movie not  found";
+	 }
+	 public String removeLineFromFile(String file, String lineToRemove) {
+
+		 try {
+
+		   File inFile = new File(file);
+
+		   if (!inFile.isFile()) {
+		     return "Parameter is not an existing file";
+		   }
+
+		   //Construct the new file that will later be renamed to the original filename.
+		   File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+
+		   BufferedReader br = new BufferedReader(new FileReader(file));
+		   PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+
+		   String line = null;
+
+		   //Read from the original file and write to the new
+		   //unless content matches data to be removed.
+		   while ((line = br.readLine()) != null) {
+
+		     if (!line.trim().equals(lineToRemove)) {
+
+		       pw.println(line);
+		       pw.flush();
+		     }
+		   }
+		   pw.close();
+		   br.close();
+		   
+		   tempFile.renameTo(inFile);
+		   
+		   //Delete the original file
+		   if (!inFile.delete()) {
+		     return "Could not delete file";
+		   }
+
+		   //Rename the new file to the filename the original file had.
+		   if (!tempFile.renameTo(inFile))
+		    return "Could not rename file";
+
+		 }
+		 catch (FileNotFoundException ex) {
+		   ex.printStackTrace();
+		 }
+		 catch (IOException ex) {
+		   ex.printStackTrace();
+		 }
+		 return "file written";
+	 }
+
+	  String  removeMovieFromInfo(String Title) throws IOException
+	 {
+		return removeMovie(Title,movieinfo);
+	 }
+	  String removeMovieFromRatings(String Title) throws IOException
+	 {
+		 return removeMovie(Title,movieRatings);
+	 }
+	 public String getDirector(String Movie) throws IOException
+	 {
+		 readMovieInfo();
+		 String DirectorName = directorMovie.get(Movie);
+		 if (DirectorName != null)
+		 {
+			 return DirectorName;
+		 }
+		 else
+		 {
+			 return "We can't find the movie in our database";
+		 }
+
+	 }
+	 public String getBestMovie(String Movie) throws IOException
+	 {
+		 List<String> moviesList = yearsMovie.get(Movie);
+		 if(moviesList != null)
+		 {
+			 Double high_rate = (double) 0 ;
+			 String MovieReq = null;
+			 for(int i = 0; i < moviesList.size(); i++)
+			 {
+				 Double rate = ratingMoive.get(moviesList.get(i));
+				 if(rate > high_rate)
+				 {
+					 MovieReq = moviesList.get(i);
+					 high_rate = rate;
+				 }
+				 
+			 }
+			 return  MovieReq;
+		 }
+		 else
+		 {
+			return "not found" ;
+		 }
+	 }
+	 public String addMovieInfo(String Movie) throws IOException
+	 {
+	 			 String[] check = Movie.split(":");
+				 if(check.length < 4)
+				 {
+					return "You are supposed to input in the way";
+				 }
+				 else
+				 {
+					 addNewMovieInfo(Movie);
+					 return "Added movie"
+				;				 }
+
+	 }
+	 public String addMovierating(String Movie) throws IOException
+	 {
+				 String[] check = Movie.split(":");
+				 if(check.length < 2)
+				 {
+					 return "You are supposed to input in the way";
+				 }
+				 else
+				 {
+					 addNewMovieRating(Movie);
+					 return "ADDEd movie";
+				 }
+	 }
+	 public String removeMovieInfo(String queryString) throws IOException
+	 {
+	 			String h = removeMovieFromInfo(queryString);
+				 directorMovie.clear();
+				 actorsMovie.clear();
+				 yearsMovie.clear();
+				 movieVars.clear();
+				 ratingMoive.clear();
+				 
+				 readMovieRatings();
+				 readMovieInfo();
+
+				 return h;
+	 }
+	 public String removeMovieRating(String queryString) throws IOException
+	 {
+	 			String h = 	removeMovieFromRatings(queryString);
+				 directorMovie.clear();
+				 actorsMovie.clear();
+				 yearsMovie.clear();
+				 movieVars.clear();
+				 ratingMoive.clear();
+				 readMovieRatings();
+				 readMovieInfo();
+				 return h;
+	 }
+	 public String changeRating (String queryString) throws IOException
+	 {
+	 			 String vars[] = queryString.split(":");
+				 if (vars.length < 2)
+				 {
+					 return "Not an expected format";
+				 }
+				 else
+				 {
+					 removeMovieFromRatings(vars[0]);
+					 directorMovie.clear();
+					 actorsMovie.clear();
+					 yearsMovie.clear();
+					 movieVars.clear();
+					 ratingMoive.clear();
+					 readMovieRatings();
+					 readMovieInfo();
+					 addNewMovieRating(queryString);
+					 return "Movie Rating Changed";
+				 }
+	 }
+	 public String changeInfo(String queryString) throws IOException
+	 {
+
+			String vars[] = queryString.split(":");
+		 if (vars.length < 4)
+		 {
+			 return "Not an expected format";
+		 }
+		 else
+		 {
+			 removeMovieFromInfo(vars[0]);
+			 directorMovie.clear();
+			 actorsMovie.clear();
+			 yearsMovie.clear();
+			 movieVars.clear();
+			 ratingMoive.clear();
+			 readMovieRatings();
+			 readMovieInfo();
+			 addNewMovieInfo(queryString);
+			 return "Movie Info Changed";
+		 }
+
+	 }
+
+
+
+}
